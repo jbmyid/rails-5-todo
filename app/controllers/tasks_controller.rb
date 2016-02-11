9fc82e745ace07b1a50f08a053e9868d55c21b7a
Jalendra Bhanarkar <jalendra@promobitech.com>
2016-02-11 12:36:23 +0530

    add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,14 +2,17 @@ class TasksController < ApplicationController
 
   respond_to :json
 
+  def create
+    @task = Task.new
+    @task.save
+    respond_with @task
+  end
+
   # GET /tasks/1
   # GET /tasks/1.json
   def show
-    # @task = Task.find_or_create_by(name: 'First')
-    @task = Task.new
-    @task.save
-    # render json: @task, serializer: TaskSerializer
-    respond_with @task #, serializer: TaskSerializer
+    @task = Task.find_or_create_by(name: 'First')
+    respond_with @task , serializer: TaskSerializer
   end
 
 end
